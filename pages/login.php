132d081b4bfcd1cd237d6672575fa280168a2ad0
uweradiane <uweradiane27@gmail.com>
2023-11-10 09:21:16 -0500

    ajout formulaire sur login

--- pages/login.php
+++ pages/login.php
@@ -1,2 +1,14 @@
 <h2> Connection<h2>
 <a href="../"> Accueil</a>
+<form method="post" action="../results/loginResult.php"> 
+    <label for="user_name">nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value=""><br>
+
+    <label for="password">mot de passe</label>
+    <input id="password" type="text" name="password" value=""><br>
+
+    <button type="submit"> Se Connecter</button>
+
+
+
+</form>
--- pages/signUp.php
+++ pages/signUp.php
@@ -1,2 +1,17 @@
 <h2> Enregistrement</h2>
 <a href="../"> Accueil</a>
+<form method="post" action="../results/signUpResult.php"> 
+    <label for="user_name">nom d'utilisateur</label>
+    <input id="user_name" type="text" name="user_name" value=""><br>
+
+    <label for="email">Email</label>
+    <input id="email" type="text" name="email" value=""><br>
+
+    <label for="password">mot de passe</label>
+    <input id="password" type="text" name="password" value=""><br>
+
+    <button type="submit"> Enregistrer</button>
+
+
+
+</form>
